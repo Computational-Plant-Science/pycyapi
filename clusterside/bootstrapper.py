af3d5bc66f721b53ec5eb8f8ae4f9ea1f9978cfd
Chris Cotter <cottersci@users.noreply.github.com>
2019-02-26 15:26:32 -0500

    Moved to the process_sample workflow paradigm (#1)

---
+++ clusterside/bootstrapper.py
@@ -0,0 +1,42 @@
+import sys
+sys.path.append('/bootstrap_code')
+sys.path.append('/user_code')
+import os.path
+import shutil
+import json
+from process import process_sample, WORKFLOW_CONFIG
+
+'''
+    Wrapper for the workflow code. Run within the workflow's singularity container.
+
+    Loads in the data about the sample to be processed by the workflow, calls
+    the workflows's process_sample function, then deals with the workflow results.
+
+    The sample data json file (params.json) should contain:
+        sample_name (str): the name of the sample
+        sample_path (str): the path to the sample files (may be a folder or file)
+        result_path (str): path of folder to store the results file in
+        args (object): params to be passed to process_sample as a dictionary
+'''
+
+if __name__ == "__main__":
+
+    with open("params.json",'r') as fin:
+        data = json.load(fin)
+
+    sample_name = data['sample_name']
+    sample_path = data['sample_path']
+    result_path = "/results/"
+    args = data['args']
+
+    result = process_sample(sample_name,
+                            sample_path,
+                            args)
+
+    if WORKFLOW_CONFIG['output_type'] == 'file':
+        shutil.move(result,os.path.join(result_path,sample_name + "_" + result))
+    elif WORKFLOW_CONFIG['output_type'] == 'csv':
+        file_path = os.path.join(result_path,
+                                 sample_name + "_" + os.path.basename(sample_name) + ".json")
+        with open(file_path,"w") as fout:
+            json.dump(result, fout)
---
+++ clusterside/data.py
@@ -0,0 +1,118 @@
+import json
+import os
+from irods.session import iRODSSession
+
+def sample_factory(name,props):
+    '''
+        name: name of sample
+        props: {
+            'storage': "irods" | "local"
+            'path': 'path_to_sample'
+        }
+    '''
+    if props['storage'] == 'irods':
+        return iRODSSample(name,props['path'])
+
+class Sample():
+    '''
+        A sample to be processed by process_sample
+
+        Attributes:
+            name (str): sample name
+            path (str): permanent storage path.
+    '''
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+
+    def get(self, path):
+        '''
+            Copy the sample file from
+        '''
+        pass
+
+    def __str__(self):
+        return "%s @ %s" % (self.name, self.path)
+
+class iRODSSample(Sample):
+
+    def __init__(self, name, path, env_file = None):
+        super().__init__(name,path)
+
+        if not env_file:
+            try:
+                self.env_file = os.environ['IRODS_ENVIRONMENT_FILE']
+            except KeyError:
+                self.env_file = os.path.expanduser('~/.irods/irods_environment.json')
+
+
+    def get(self, to_path = "./"):
+        session  = iRODSSession(irods_env_file=self.env_file)
+
+        if session.data_objects.exists(self.path):
+            local_path = os.path.join(to_path, os.path.basename(self.path))
+            session.data_objects.get(self.path, file=local_path)
+        elif session.collections.exists(self.path):
+            if recursive:
+                coll = session.collections.get(self.path)
+                local_path = os.path.join(local_path, os.path.basename(self.path))
+                os.mkdir(local_path)
+
+                for file_object in coll.data_objects:
+                    get(session, os.path.join(self.path, file_object.path), local_path, True)
+                for collection in coll.subcollections:
+                    get(session, collection.path, local_path, True)
+            else:
+                raise FileNotFoundError("Skipping directory " + self.path)
+        else:
+            raise FileNotFoundError(self.path + " Does not exist")
+
+        return local_path
+
+
+class Workflow():
+    '''
+        Contains information about the workflow to be run
+
+        Attributes:
+            output_type (str): the type of value returned by process_sample. See
+                WORKFLOW_CONFIG docs for more info.
+            singularity_url (str): the url to the singularity container that
+                process sample is run in.
+            auth_token (str): The authorization token required to communicate
+                to the web server via the REST API
+            job_pk (int): the job pk
+            server_url (str): The url to the webserver REST api
+            args (dict): the arguments that are passed to process_sample
+    '''
+    def __init__(self, config, json_file):
+        with open(json_file,"r") as fin:
+            params = json.load(fin)
+
+        self.output_type = config['output_type']
+        self.singularity_url = config['singularity_url']
+        self.auth_token = params['auth_token']
+        self.job_pk = params['job_pk']
+        self.server_url = params['server_url']
+
+        self.args = params['parameters']
+
+class Collection():
+    '''
+        Contains information about the collection of samples to be processed.
+    '''
+    def __init__(self, json_file):
+        '''
+            Args:
+                json_file (str): path to the json file containing information
+                    about the samples
+        '''
+        with open(json_file,"r") as fin:
+            self.__samples__ = json.load(fin)
+
+    def samples(self):
+        """
+            Returns a generator of samples within the collection.
+        """
+        for name,sample in self.__samples__.items():
+            yield sample_factory(name,sample)
