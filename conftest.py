4a0370b938541e80152e8fb64c234e3bab0f8fc1
Wes Bonelli <wpb3hw@virginia.edu>
2020-04-10 18:29:23 -0400

    debug .travis.yml (install pytest)

--- conftest.py
+++ conftest.py
@@ -1,14 +1,16 @@
 # conftest.py
 import sys
 
-#Mock the process.py module usually provided by PlantIT before calling
-#clusterside. Required to run test_bootstrapper.py
-def process_sample(name,path,args):
+
+# Mock the process.py module usually provided by PlantIT before calling
+# clusterside. Required to run test_bootstrapper.py
+def process_sample(name, path, args):
     from pathlib import Path
     Path('a_really_awecsome_file.txt').touch()
     return {
         'key-val': {'name': name, 'path': path}
     }
+
 
 module = type(sys)('process')
 module.process_sample = process_sample
