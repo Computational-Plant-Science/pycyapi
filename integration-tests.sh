11e623f6eec9c31ea9a5c544c39f2faae468e0cd
Wes Bonelli <wpb3hw@virginia.edu>
2020-06-04 19:38:48 -0400

    remove IRODS deps from integration test (add later once reimplemented), disable unit test script until tests added

--- integration-tests.sh
+++ integration-tests.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-echo "Waiting for IRODS to start..."
-
-./wait-for-postgres.sh irods
-./wait-for-it.sh irods:1247 --
+# echo "Waiting for IRODS to start..."
+# ./wait-for-postgres.sh irods
+# ./wait-for-it.sh irods:1247 --
 
 echo "Running integration tests..."
-
-iput /opt/plantit-clusterside/tests/sample1.jpg
-iput /opt/plantit-clusterside/tests/sample2.jpg
-iput /opt/plantit-clusterside/tests/sample3.jpg
-
 python3 /opt/plantit-clusterside/tests/test.py
