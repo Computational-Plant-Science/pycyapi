ea896537b850ce7d37f0b7abf288c11a007435fc
Wes Bonelli <wpb3hw@virginia.edu>
2020-06-04 20:06:03 -0400

    fix .travis.yml

--- unit-tests.sh
+++ unit-tests.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 echo "Waiting for IRODS to start..."
-
 ./wait-for-postgres.sh irods
 ./wait-for-it.sh irods:1247 --
 
 echo "Running unit tests..."
-
 cd /opt/plantit-clusterside/ || exit
 python3 -m pytest
