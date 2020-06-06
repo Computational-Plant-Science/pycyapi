546045a516582b49502a7f3ac875a63d960a643c
Wes Bonelli <wpb3hw@virginia.edu>
2020-06-06 16:11:45 -0400

    fix .travis.yml and remove unneeded unit test script

--- unit-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Waiting for IRODS to start..."
-./wait-for-postgres.sh irods
-./wait-for-it.sh irods:1247 --
-
-echo "Running unit tests..."
-cd /opt/plantit-clusterside/ || exit
-python3 -m pytest
