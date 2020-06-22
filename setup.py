f119dbffadc7d46b23100bc26e5c24e67ac7bdf5
Wes Bonelli <wpb3hw@virginia.edu>
2020-06-21 22:52:40 -0400

    0.0.5 (find packages to include in distribution automatically and other debugging)

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
-setup(name='plantit-cluster',
-      version='0.0.4',
-      description='PlantIT workflow management CLI.',
-      author='Computational Plant Science Lab',
-      author_email='wbonelli@uga.edu',
-      license='BSD-3-Clause',
-      url='https://github.com/Computational-Plant-Science/plantit-cluster',
-      packages=['plantit_cluster'],
-      entry_points={
-          'console_scripts': [
-              'plantit = plantit_cluster.cluster:cli'
-          ]
-      },
-      python_requires='>=3.6.9',
-      install_requires=['requests', 'python-irodsclient', 'dask', 'dask-jobqueue', 'dagster', 'dagster-dask', 'pyaml'],
-      setup_requires=['wheel'],
-      tests_require=['pytest'])
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='plantit-cluster',
+    version='0.0.5',
+    description='PlantIT workflow management CLI.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Computational Plant Science Lab',
+    author_email='wbonelli@uga.edu',
+    license='BSD-3-Clause',
+    url='https://github.com/Computational-Plant-Science/plantit-cluster',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'plantit = plantit_cluster.cluster:cli'
+        ]
+    },
+    python_requires='>=3.6.9',
+    install_requires=['requests', 'python-irodsclient', 'dask', 'dask-jobqueue', 'dagster', 'dagster-dask',
+                      'pyaml'],
+    setup_requires=['wheel'],
+    tests_require=['pytest'])
