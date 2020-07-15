2c311a863f1746dbe15271c5ae3ef4e487161b11
Wes Bonelli <wpb3hw@virginia.edu>
2020-07-14 20:06:52 -0400

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='plantit-cli',
-    version='0.0.12',
+    version='0.0.13',
     description='PlantIT workflow management CLI.',
     long_description=long_description,
     long_description_content_type="text/markdown",
