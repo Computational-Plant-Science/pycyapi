50113d46af02f4d5aab204cc70a513f048a13e42
Wes Bonelli <wpb3hw@virginia.edu>
2020-10-06 16:17:56 -0400

    fix create-env-file.sh, bump

--- scripts/create-env-file.sh
+++ scripts/create-env-file.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 if [[ -z "${CYVERSE_USERNAME}" ]]; then
-  github_secret="some_cyverse_username"
+  cyverse_username="some_cyverse_username"
   echo "Warning: CYVERSE_USERNAME environment variable missing"
 else
-  github_secret="${CYVERSE_USERNAME}"
+  cyverse_username="${CYVERSE_USERNAME}"
 fi
 
 if [[ -z "${CYVERSE_PASSWORD}" ]]; then
-  cas_server_url="some_cyverse_password"
+  cyverse_password="some_cyverse_password"
   echo "Warning: CYVERSE_PASSWORD environment variable missing"
 else
-  cas_server_url="${CYVERSE_PASSWORD}"
+  cyverse_password="${CYVERSE_PASSWORD}"
 fi
 
 cat <<EOT >>".env"
-CYVERSE_USERNAME=
-CYVERSE_PASSWORD
+CYVERSE_USERNAME=$cyverse_username
+CYVERSE_PASSWORD=$cyverse_password
 EOT
