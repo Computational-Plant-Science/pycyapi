3bdac48203c8fbaf3eb6f7f28657c2f697436fa0
Wes Bonelli <wpb3hw@virginia.edu>
2020-10-22 21:17:38 -0400

    fix executor access in entrypoint and bump

--- plantit_cli/cli.py
+++ plantit_cli/cli.py
@@ -30,7 +30,7 @@ def run(workflow, plantit_token, cyverse_token):
         if 'local' in executor_def:
             LocalExecutor(cyverse_token).execute(Run(**workflow_def))
         elif 'jobqueue' in executor_def and 'slurm' in executor_def['jobqueue']:
-            executor_def = dict(executor_def['slurm'])
+            executor_def = dict(executor_def['jobqueue']['slurm'])
             JobQueueExecutor('slurm', cyverse_token, **executor_def).execute(Run(**workflow_def))
         else:
             raise ValueError(f"Unrecognized executor (supported: 'local', 'jobqueue')")
