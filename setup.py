f392f9524bc8e72a3aadcaee50e2de606b006718
Wes Bonelli <wpb3hw@virginia.edu>
2020-11-16 09:49:25 -0500

    slightly longer delay between tests

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='plantit-cli',
-    version='0.1.4',
+    version='0.1.5',
     description='Deploy PlantIT workflows on laptops, servers, or HPC/HTC clusters.',
     long_description=long_description,
     long_description_content_type="text/markdown",
