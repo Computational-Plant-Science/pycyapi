18d12fddff82bce2df6f8f0ad752e7359780c31a
Wes Bonelli <wpb3hw@virginia.edu>
2021-01-12 10:42:49 -0500

    minor fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='plantit-cli',
-    version='0.1.125',
+    version='0.1.126',
     description='Deploy PlantIT workflows on laptops, servers, or clusters.',
     long_description=long_description,
     long_description_content_type="text/markdown",
