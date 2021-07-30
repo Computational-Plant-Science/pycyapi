a2fc902cc72008b294bac4ad6f311dcac6e6a0f3
Wes Bonelli <wpb3hw@virginia.edu>
2021-07-29 21:40:45 -0400

    pass params as env vars into singularity context

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='plantit-cli',
-    version='0.2.87',
+    version='0.2.88',
     description='Deploy PlantIT workflows on laptops, servers, or clusters.',
     long_description=long_description,
     long_description_content_type="text/markdown",
