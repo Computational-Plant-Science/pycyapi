92d833f7f0b9f1c71cfe65d63c165296f85a24a0
Wes Bonelli <wpb3hw@virginia.edu>
2021-09-21 10:48:13 -0400

    debug CI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='plantit-cli',
-    version='0.3.8',
+    version='0.3.9',
     description='Deploy PlantIT workflows on laptops, servers, or clusters.',
     long_description=long_description,
     long_description_content_type="text/markdown",
